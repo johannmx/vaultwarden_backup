2d8f0143c9d5e90a3058e2fb5cf15d0c04476018
Johann Medina <johann.medina@lamercantilandina.com.ar>
2022-04-12 14:57:56 -0300

    Agrego curl para Gotify

--- script.sh
+++ script.sh
@@ -42,4 +42,4 @@ fi
 
 echo "[$(date +"%F %r")] ${OUTPUT}."
 echo "[$(date +"%F %r")] Sending notification to Gotify Server."
-curl "https://push.negrura.ar/message?token=APHA80R8HVKdTZN" -F "title=Vaultwarden Backup" -F "message=${OUTPUT}" -F "priority=5" # Send message to Gotify
+curl "https://${GOTIFY_SERVER}/message?token=${GOTIFY_TOKEN}" -F "title=Vaultwarden Backup" -F "message=${OUTPUT}" -F "priority=5" # Send message to Gotify
