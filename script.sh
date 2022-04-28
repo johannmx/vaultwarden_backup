620507d45d9f129c5fdcca567ae7c3a8052f2a6d
Johann Medina <johann.medina@lamercantilandina.com.ar>
2022-04-28 13:24:40 -0300

    telegram fix

--- script.sh
+++ script.sh
@@ -44,9 +44,9 @@ echo "[$(date +"%F %r")] ${OUTPUT}."
 echo "[$(date +"%F %r")] Sending notification to Gotify Server."
 #curl "https://${GOTIFY_SERVER}/message?token=${GOTIFY_TOKEN}" -F "title=Vaultwarden Backup" -F "message=${OUTPUT}" -F "priority=5" # Send message to Gotify
 
-apprise -vv -t "💾 Status Backup Vaultwarden" -b "☑️ ${OUTPUT}" \
+apprise -vv -t "Backup Vaultwarden" -b "☑️ 💾 ${OUTPUT}" \
    "gotifys://${GOTIFY_SERVER}/${GOTIFY_TOKEN}/?priority=high"
 
 echo "[$(date +"%F %r")] Sending notification to Telegram."
-apprise -vv -t "💾 Status Backup Vaultwarden" -b "☑️ ${OUTPUT}" \
-   "tgram://${TGRAM_BOT_TOKEN}/${TGRAM_CHAT_ID}/?image=Yes"
+apprise -vv -t "💾 Backup Vaultwarden" -b "☑️ ${OUTPUT}" \
+   "tgram://${TGRAM_BOT_TOKEN}/${TGRAM_CHAT_ID}"
